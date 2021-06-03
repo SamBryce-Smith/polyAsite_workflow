97752bf76698de3426e4c8a2d3937d05e5e09576
Sam Bryce-Smith <49978382+SamBryce-Smith@users.noreply.github.com>
2021-06-03 17:54:30 +0100

    barebones submit script for ucl cs cluster. Untested

---
+++ sge_ucl_submit.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#Submit to the cluster, give it a unique name
+#$ -S /bin/bash
+
+#$ -cwd
+#$ -V
+#$ -l h_vmem=1.9G,h_rt=20:00:00,tmem=1.9G
+#$ -pe smp 2
+
+# join stdout and stderr output
+#$ -j y
+#$ -R y
+
+
+snakemake \
+-p \
+--configfile config.yaml \
+--use-singularity \
+--jobscript cluster_qsub.sh \
+--singularity-args "--bind ${PWD}" \
+--cluster-config cluster_config.json \
+--cluster-sync "qsub -R y -l tmem={cluster.mem},h_vmem={cluster.mem},h_rt={cluster.time} -pe smp {cluster.threads} -o {params.cluster_log}" \
+-j 40 \
+--nolock \
+--rerun-incomplete \
+--latency-wait 100 \
+--local-cores 1 \
+--until complete_clustering
